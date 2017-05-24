5f86b92f0cc5131155705012f66f820d5d9a77e4
hfl-long <794657496@qq.com>
2017-05-24 22:47:59 +0800

    springboot的aop处理请求

--- src/main/java/com/hfl/domain/Girl.java
+++ src/main/java/com/hfl/domain/Girl.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 /**
  * @Author:黄飞龙
@@ -18,6 +19,7 @@ public class Girl {
     private String cupSize;
     @Min(value = 18,message = "未成年少女禁止入门")
     private Integer age;
+    @NotNull(message = "金额必传")
     private Double money;
 
     public Double getMoney() {
